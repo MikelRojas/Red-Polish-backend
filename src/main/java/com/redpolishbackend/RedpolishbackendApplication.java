b164f5b42433e59542d63582ad1d90fce5b6d20a
mikelrojasr <mikelrojasrodriguez@hotmail.com>
2026-02-24 08:45:36 -0600

    add the last changues

--- src/main/java/com/redpolishbackend/RedpolishbackendApplication.java
+++ src/main/java/com/redpolishbackend/RedpolishbackendApplication.java
@@ -21,7 +21,7 @@ public class RedpolishbackendApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
